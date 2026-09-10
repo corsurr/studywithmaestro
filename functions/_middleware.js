f6c8a311f5d7e7d6d7d42bcd5402c9394b28a901
corsurr <mrcarlharrisz@gmail.com>
2026-09-10 14:11:32 +0800

    Add MAESTRO source file: functions/_middleware.js

---
+++ functions/_middleware.js
@@ -0,0 +1,8 @@
+export async function onRequest(context) {
+  const response = await context.next();
+  const headers = new Headers(response.headers);
+  headers.set('X-Content-Type-Options', 'nosniff');
+  headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+  headers.set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+  return new Response(response.body, { status: response.status, statusText: response.statusText, headers });
+}
